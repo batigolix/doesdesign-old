af2f11209d45674733ebb7c59d8a52f337c999e9
Boris Doesborg <bdoesborg@gmail.com>
2016-06-18 11:33:19 +0200

    wip setting up website

---
+++ aliases.drushrc.php
@@ -0,0 +1,9 @@
+<?php
+$aliases['dd8.val'] = array(
+  'parent' => '@val',
+  'uri' => 'dd8.val',
+  'root' => '/Users/boris/Sites/doesdesign/web',
+  'path-aliases' => array(
+    '%files' => 'sites/default/files',
+  ),
+);
